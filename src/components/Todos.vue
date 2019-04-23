c261d92d5be4fe05c656834b751f2285361c9e79
haidi20 <nhaidii@yahoo.com>
2019-04-24 05:41:02 +0800

    update-delete

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,19 +3,20 @@
 		<h3>Todos</h3>
 		<div class="todos">
 			<div v-for="todo in allTodos" :key="todo.id" class="todo">
-				{{todo.title}}				
+				{{todo.title}}	
+				<i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>			
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import {mapGetters, mapActions} from 'vuex'
+	import {mapGetters, mapActions} from 'vuex'	
 
 	export default{
 		name: 'Todos',
 		methods: {
-			...mapActions(['fetchTodos'])
+			...mapActions(['fetchTodos', 'deleteTodo'])
 		},
 		computed: mapGetters(['allTodos']),
 		created(){
@@ -25,6 +26,7 @@
 </script>
 
 <style scoped>
+
  	.todos{
  		display: grid;
  		grid-template-columns: repeat(3, 1fr);
@@ -37,6 +39,13 @@
  		border-radius: 5px;
  		text-align: center;
  		position: relative;
+ 		cursor: pointer;
+ 	}
+ 	i{
+ 		position:absolute;
+ 		bottom: 10px;
+ 		right: 10px;
+ 		color: #fff;
  		cursor: pointer;
  	}
 </style>
