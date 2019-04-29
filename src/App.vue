ecf29f89aa5d08af12f5f4e0d06333b0d91100ab
haidi20 <nhaidii@yahoo.com>
2019-04-30 05:37:47 +0800

    update-

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
   <div id="app">
     <div class="container">
-      <AddTodo />
-      <FilterTodos />
-      <Todos />
+      hello world
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
-import AddTodo from './components/AddTodo.vue'
-import FilterTodos  from './components/FilterTodos.vue'
 
 export default {
   name: 'app',
-  components: {
-    Todos,
-    AddTodo,
-    FilterTodos
-  }
 }
 </script>
 
