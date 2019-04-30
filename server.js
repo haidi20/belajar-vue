8d3b197ad524204a92f04b00eb11823a182d5691
haidi20 <nhaidii@yahoo.com>
2019-05-01 05:40:32 +0800

    work-run-apollo-server-test-query

---
+++ server.js
@@ -0,0 +1,31 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const todos = [
+	{task : 'Wash Car', completed: false},
+	{task : 'Clean Room', completed: true}
+]
+
+const typeDefs = gql`
+	type Todo {
+		task: String
+		completed: Boolean
+	}
+
+	type Query { 
+		getTodos: [Todo]
+	}
+`
+
+const resolvers = {
+	Query: {
+		getTodos:() => todos
+	}
+}
+
+const server = new ApolloServer({
+	typeDefs, resolvers
+});
+
+server.listen().then(({ url }) => {
+	console.log(`server listening ${url}`);
+});
