38f692e4ee0e3798a1efe227c06fafc7bdfa7e67
haidi20 <nhaidii@yahoo.com>
2019-05-01 06:35:18 +0800

    update-connect-db-mongoose

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variable.env' });
 
 mongoose
-	.connect(process.env.MONGO_URI)
+	.connect(
+		process.env.MONGO_URI,
+		{ useNewUrlParser: true }
+	)
 	.then(() => console.log('DB conected mantul'))
 	.catch(err => console.error(err));
 
