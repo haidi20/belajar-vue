79bca4995349b9f753765f9a0250b22383284df9
haidi20 <nhaidii@yahoo.com>
2019-05-01 06:04:06 +0800

    update-function-addTodo

--- server.js
+++ server.js
@@ -14,11 +14,24 @@ const typeDefs = gql`
 	type Query { 
 		getTodos: [Todo]
 	}
+
+	type Mutation {
+		addTodo(task: String, completed: Boolean): Todo
+	}
 `
 
 const resolvers = {
 	Query: {
 		getTodos:() => todos
+	},
+
+	Mutation: {
+		addTodo: (_, {task, completed}) => {
+			const todo = {task, completed} 
+			todos.push(todo)
+
+			return todo
+		}
 	}
 }
 
