262c172709694d8b00b714dcf3714ff4beb0975b
haidi20 <nhaidii@yahoo.com>
2019-05-03 04:52:15 +0800

    update-install-vuetify

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <h1>App</h1>
+    <router-view />
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+// import colors from 'vuetify/es5/util/colors'
+
+
+Vue.use(Vuetify, {
+  	iconfont: 'md',
+  	// theme:{
+   //    primary: colors.indigo.base,
+   //    secondary: colors.blue.base,
+   //    accent: colors.deep-orange.base,
+   //    error: colors.red.base,
+   //    warning: colors.yellow.base,
+   //    info: colors.cyan.base,
+   //    success: colors.green.base
+   //   }
+})
